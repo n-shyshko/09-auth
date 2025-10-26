0cfc27742ccfacecc93135c1322fad79937ac9d7
nik <nshishko@gmail.com>
2025-10-26 11:11:25 +0200

    fix 5

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -27,7 +27,7 @@ export async function generateMetadata({
       title: `Notes: ${tag}`,
       description:
         "Browse your notes, stay organized, and manage your ideas with NoteHub.",
-      url: `https://09-auth-theta-one.vercel.app/${slug[0]}`,
+      url: `https://08-zustand-green-rho.vercel.app/${slug[0]}`,
       images: [
         {
           url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
