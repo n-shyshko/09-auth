19f6693d7ed6d8ea5c164ee37905804c9085951e
nik <nshishko@gmail.com>
2025-10-26 10:55:40 +0200

    fix 5

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,17 @@
+// import axios from "axios";
+
+// const baseURL = process.env.NEXT_PUBLIC_API_URL
+//   ? `${process.env.NEXT_PUBLIC_API_URL.replace(/\/$/, "")}/api`
+//   : "/api";
+
+// export const nextServer = axios.create({
+//   baseURL,
+//   withCredentials: true,
+// });
+
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL
-  ? `${process.env.NEXT_PUBLIC_API_URL.replace(/\/$/, "")}/api`
-  : "/api";
-
 export const nextServer = axios.create({
-  baseURL,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   withCredentials: true,
 });
